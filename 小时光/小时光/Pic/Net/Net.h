e5063a80cfc6384dafc4b9f033f5b331f4f1b04d
xinghua5 <xinghua5@bj-m-203827a.local>
2016-11-13 16:12:14 +0800

    no message

--- 小时光/小时光/Pic/Net/Net.h
+++ 小时光/小时光/Pic/Net/Net.h
@@ -10,7 +10,7 @@
 #import "AFNetworking.h"
 //test
 double m;
-//bierenxiugai
+//bierenxiugai1
 typedef void(^success) (id response);
 typedef void(^fail) (NSError* error);
 @interface Net : NSObject
